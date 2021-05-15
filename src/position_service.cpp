38e0dbed16cb804e4fcbeaaceae2ae41837cb515
Alessio <alessioroda98@gmail.com>
2021-05-15 23:28:44 +0200

    work in proggress

--- src/position_service.cpp
+++ src/position_service.cpp
@@ -1,6 +1,58 @@
 #include"rclcpp/rclcpp.hpp"
-#include "rt2_assignment1/src/RandomPosition.hpp"
+#include "rt2_assignment1/srv/random_position.hpp"
 
+using std::placeholders::_1;
+
+namespace rt2_assignment1
+{
+    class RandomServer : public rclcpp::Node
+    {
+        public:
+
+            RandomServer(const rclcpp::NodeOptions & options)
+            : Node("position_service_component")
+            { 
+            //initialize the publisher, the subscriber, client1, client2
+            service=this->create_service<rt2_assignment1::srv::RandomPosition>("/position_server", std::bind(&StateMachine::myrandom, this, _1));
+
+            this->myrandom();
+
+            }
+
+
+        private:
+
+            rclcpp::Service<rt2_assignment1::srv::Command>::SharedPtr service;
+
+            bool myrandom(
+            const std::shared_ptr<rmw_request_id_t> request_header,
+            const std::shared_ptr<rt2_assignment1::srv::RandomPosition::Request> req,
+            const std::shared_ptr<rt2_assignment1::srv::RandomPosition::Response> res)
+             {
+                    (void)request_header;
+
+                     res.x = randMToN(req.x_min, req.x_max);
+                     res.y = randMToN(req.y_min, req.y_max);
+                     res.theta = randMToN(-3.14, 3.14);
+                     return true;
+             };
+
+            double randMToN(double M, double N)
+             {     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
+    }
+}
+
+RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::RandomServer)
+
+
+
+
+
+
+
+
+
+/*
 
 double randMToN(double M, double N)
 {     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
@@ -23,3 +75,4 @@ int main(int argc, char **argv)
 
    return 0;
 }
+*/
--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -2,63 +2,93 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <cinttypes>
+#include <cstdlib>
 
 #include"rclcpp/rclcpp.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
-#include "rt2_assignment1/src/Position.hpp"
-#include "rt2_assignment1/src/Command.hpp"
-#include "rt2_assignment1/src/RandomPosition.hpp"
+#include "rt2_assignment1/srv/position.hpp"
+#include "rt2_assignment1/srv/command.hpp"
+#include "rt2_assignment1/srv/random_position.hpp"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 namespace rt2_assignment1
 {
+
+    class StateMachine : public rclcpp::Node
+    {
+
+        public: 
+
+            StateMachine(const rclcpp::NodeOptions & options)
+            : Node("state_machine_component")
+            { 
+            //initialize the publisher, the subscriber, client1, client2
+            service=this->create_service<rt2_assignment1::srv::Command>("/user_interface", std::bind(&StateMachine::user_interface, this, _1));
+            client_p = this->create_client<rt2_assignment1::srv::Position>("/go_to_point");
+            clinet_random= this->create_client<rt2_assignment1::srv::RandomPosition("/position_server");
+
+
+            this->position_server_clbk();
+
+            }
+
+
+        void position_server_clbk(){
+            auto req=std::make_shared<rt2_assignment1::srv::RandomPosition::Request>();
+            req->x_max=5.0;
+            req->x_min=-5.0;
+            req->y_max=5.0;
+            req->y_min=-5.0;
+
+            using ServiceResponseFuture = rclcpp::Client<rt2_assignment1::srv::RandomPosition>::SharedFuture;
+            auto received_callback=[this](ServiceResponseFuture future){
+            
+            if(start)
+            {
+                auto req= std::make_shared<rt2_assignment1::srv::Position::Request>();
+                req->x=future.get()->x;
+                req->y=future.get()->y;
+                req->theta=future.get()->theta;
+                auto send_result= client_p->async_send_request(req);
+            }
+            
+            };
+            auto send_result= client_random->async_send_request(req, received_callback);
+        }
+
+
+        private:
+
+            bool start=false;
+
+            rclcpp::Client<rt2_assignment1::srv::Position>::SharedPtr client_p;
+            rclcpp::Client<rt2_assignment1::srv::RandomPosition>::SharedPtr clinet_random;
+            rclcpp::Service<rt2_assignment1::srv::Command>::SharedPtr service;
+
+
+
+            void user_interface(
+            const std::shared_ptr<rmw_request_id_t> request_header,
+            const std::shared_ptr<rt2_assignment1::srv::Command::Request> req,
+            const std::shared_ptr<rt2_assignment1::srv::Command::Response> res)
+             {
+                (void)request_header;
+                
+                    if (req.command == "start"){
+                        start = true;
+                    }
+                    else {
+                        start = false;
+                    }
+
+                }
+                
+            };
     
-class MinimalServer : public 
-
-bool start = false;
-
-bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res){
-    if (req.command == "start"){
-    	start = true;
-    }
-    else {
-    	start = false;
-    }
-    return true;
 }
 
-
-int main(int argc, char **argv)
-{
-   ros::init(argc, argv, "state_machine");
-   ros::NodeHandle n;
-   ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
-   ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
-   ros::ServiceClient client_p = n.serviceClient<rt2_assignment1::Position>("/go_to_point");
-   
-   rt2_assignment1::RandomPosition rp;
-   rp.request.x_max = 5.0;
-   rp.request.x_min = -5.0;
-   rp.request.y_max = 5.0;
-   rp.request.y_min = -5.0;
-   rt2_assignment1::Position p;
-   
-   while(ros::ok()){
-   	ros::spinOnce();
-   	if (start){
-   		client_rp.call(rp);
-   		p.request.x = rp.response.x;
-   		p.request.y = rp.response.y;
-   		p.request.theta = rp.response.theta;
-   		std::cout << "\nGoing to the position: x= " << p.request.x << " y= " <<p.request.y << " theta = " <<p.request.theta << std::endl;
-   		client_p.call(p);
-   		std::cout << "Position reached" << std::endl;
-   	}
-   }
-   return 0;
-   }
-}
-
-RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::MinimalServer)
+RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::StateMachine)
 
